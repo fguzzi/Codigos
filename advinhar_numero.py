d883cfb09dea9fe8b0c4c7e93a27d21cbfd926ed
faguzzi <aurelio_guzzi@yahoo.com.br>
2022-08-20 10:32:29 -0300

    vinteagosto

--- advinhar_numero.py
+++ advinhar_numero.py
@@ -1,16 +1,11 @@
-print("JOGO DE ADVINHAÇÃO")
-
+print("--------JOGO DE ADVINHAÇÃO--------")
 from random import randint
-
-
-
 print('EU PENSEI EM UM NUMERO DE 1 A 20.')
 print('VOCÊ TERÁ 3 TENTATIVAS PARA ADVINHAR.')
-palpite = 0
-tentativas = 0
-
 while True:
     sorteio = randint(1, 20)
+    palpite = 0
+    tentativas = 0
     while True:
         palpite = int(input('SEU PALPITE: '))
         if sorteio > palpite:
@@ -23,12 +18,9 @@ while True:
             print(f' EU ESCOLHI {sorteio} ! PARABÉNS, VOCÊ ADVINHOU !!!')
             break
         if tentativas == 3:
-            print('VOCÊ ESGOTOU 3 TENTATIVAS!')
-            print(f' O número que pensei foi {sorteio}.')
+            print(f'VOCÊ ESGOTOU 3 TENTATIVAS! O NÚMERO ERA {sorteio}')
             break
     op = str(input('DESEJA JOGAR NOVAMENTE? [ S/N ]: ')).strip().upper()
     if op == 'N':
         print('ATÉ LOGO !!')
         break
-
-
