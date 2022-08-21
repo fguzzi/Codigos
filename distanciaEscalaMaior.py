48de8a36f0e47308c892ae6180ba6984cda02905
faguzzi <aurelio_guzzi@yahoo.com.br>
2022-08-21 10:35:20 -0300

    vinteumagosto

--- distanciaEscalaMaior.py
+++ distanciaEscalaMaior.py
@@ -3,10 +3,9 @@ print('--------------------------ESCALA  MAIOR---------------------------------'
 notas = ['Primeira', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sétima', 'Oitava']
 dist = [1,2,2.5, 3.5, 4.5, 5.5, 6]
 while True:
-    print('''Qual a posição da 2ª nota na escala?
-               [ 1 ] - Primeira(Tônica)
+    print('''Qual a posição da nota na escala?
                [ 2 ] - Segunda
-               [ 3 ] - Teça
+               [ 3 ] - Terça
                [ 4 ] - Quarta
                [ 5 ] - Quinta
                [ 6 ] - Sexta
@@ -17,7 +16,7 @@ while True:
     if esc == 1:
         print(f' Não há distância.')
     else:
-        print(f' A distância é de {dist[esc-2]} tons')
+        print(f' A distância é de {dist[esc-2]} tom(s)')
     op = str(input('Deseja continuar? [ S/N ]: ')).strip().upper()[0]
     if op == 'N':
         break
